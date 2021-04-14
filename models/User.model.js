a20e2611d2ffca73dc72903cb2a456a1450476a0
duynv1895 <duynv@ql6625.fr>
2021-04-14 22:28:44 +0700

    Add hashed password before save

--- models/User.model.js
+++ models/User.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -13,5 +14,27 @@ const userSchema = new Schema({
         required: true
     }
 });
+
+userSchema.pre('save', async function (next){
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const hasedPassword = await bcrypt.hash(this.password, salt);
+        this.password  = hasedPassword;
+        next();
+    } catch(error) {
+        console.log(error);
+        next(error);
+    }
+})
+
+userSchema.post('save', async function (next){
+    try {
+        console.log('Called after save');
+    } catch(error) {
+        console.log(error);
+        next(error);
+    }
+})
+
 
 module.exports = mongoose.model('User', userSchema);
