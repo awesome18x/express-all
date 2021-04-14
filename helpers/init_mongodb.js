ee9a1ba579d3637024457cd7fc9b9511c74106c3
duynv1895 <duynv@ql6625.fr>
2021-04-14 21:32:17 +0700

    Initial connect to database

---
+++ helpers/init_mongodb.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/express-all', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to mongoosedb');
+}).catch((error) => {
+    console.log(error);
+})
--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require("express");
 const createError = require("http-errors");
 const morgan = require("morgan");
 require("dotenv").config();
+require('./helpers/init_mongodb');
 
 const authRouter = require('./routes/auth.router');
 
 const app = express();
+app.use(morgan('dev'));
+
 
 app.get("/", async (req, res, next) => {
     res.send("Hello world");
